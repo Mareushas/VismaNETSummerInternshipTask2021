3dad620934872f78adde8b963977d9a9470a4ea8
Marius Grigaliūnas <77457402+Mareushas@users.noreply.github.com>
2021-06-04 11:15:26 +0300

    Initial application

---
+++ Code/User.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VismaSummerInternshipTask
+{
+    /// <summary>
+    /// Data class for user object
+    /// </summary>
+    public class User
+    {
+        public string Name { get; set; }
+        public string Lastname { get; set; }
+        public int TakenBooks { get; set; }
+
+        public User(string name, string lastname)
+        {
+            this.Name = name;
+            this.Lastname = lastname;
+            this.TakenBooks = 1;
+        }
+    }
+}
